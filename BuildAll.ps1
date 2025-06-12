e3f3ca17e8a97861fd5db3bf2d202893909bae68
bassyboi <165348445+bassyboi@users.noreply.github.com>
2025-06-13 08:44:57 +1000

    Use BuildAll script in CI

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -1,5 +1,11 @@
 param([string]$UE="C:\Program Files\Epic Games\UE_5.4")
+
+if (-not (Test-Path "$UE\Engine\Build\BatchFiles\RunUAT.bat")) {
+    throw "RunUAT.bat not found under $UE"
+}
+
 & "$UE\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin `
    -Plugin="$PSScriptRoot\Plugins\SquadAI\SquadAI.uplugin" `
-   -Package="$PSScriptRoot\Packages\SquadAI" -TargetPlatforms=Win64,Lin64
+   -Package="$PSScriptRoot\Packages\SquadAI" `
+   -TargetPlatforms=Win64
 
